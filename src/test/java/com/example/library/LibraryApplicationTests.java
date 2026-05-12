dae5cb9fd5ae2773c9f2d6c3aaeb1e7657e2d7e3
v.sathvik <sathvik_vadapalli@srm.edu.in>
2026-05-12 12:04:09 +0530

    Refactor: Move tests to correct package and add context load test

--- src/test/java/com/example/library/LibraryApplicationTests.java
+++ src/test/java/com/example/library/LibraryApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.backend;
+package com.example.library;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class LibraryApplicationTests {
 
 	@Test
 	void contextLoads() {
